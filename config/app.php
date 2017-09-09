2706d3ecc4fd9ef848854bb18e92c88c7d1cd1b3
taciobrito <tacio.7brito@gmail.com>
2017-09-09 09:02:56 -0300

    merge

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'url' => 'http://localhost',
+    'url' => 'http://localhost:8000/',
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY', 'aELIqIsvgXXz3xWWL5zba4cFP9n9pR4a'),
 
     'cipher' => 'AES-256-CBC',
 
